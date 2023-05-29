f955eb44ef38145d62162c5e0d1352bb1bdac12f
Daffa <70159376+dafx7@users.noreply.github.com>
2023-05-29 15:00:26 +0700

    updated the filters field

--- listings/filters.py
+++ listings/filters.py
@@ -4,9 +4,13 @@ from .models import *
 
 
 class ListingFilter(django_filters.FilterSet):
+    num_bedrooms = RangeFilter(field_name='num_bedrooms')
+    num_bathrooms = RangeFilter(field_name='num_bathrooms')
+    square_footage = RangeFilter(field_name='square_footage')
     price = RangeFilter(field_name='price')
 
     class Meta:
         model = Listing
         fields = '__all__'
-        exclude = ['title', 'price']
+        exclude = ['title', 'price', 'num_bedrooms',
+                   'num_bathrooms', 'square_footage']
